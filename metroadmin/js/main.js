1806889c6b0885ca49f7de804c58e80f34a95046
karthi <karthikeyanar@gmail.com>
2013-04-19 13:15:01 +0530

    custom table, table page changes

---
+++ metroadmin/js/main.js
@@ -0,0 +1,12 @@
+﻿var Main = {
+	fixedLayout: function () {
+		$(".container-fluid").each(function () {
+			$(this).removeClass("container-fluid").addClass("container");
+		});
+	}
+	, fluidLayout: function () {
+		$(".container").each(function () {
+			$(this).removeClass("container").addClass("container-fluid");
+		});
+	}
+}
